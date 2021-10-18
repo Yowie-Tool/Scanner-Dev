962e9669f0a22ed668cc783eea57c9625540f4bc
Adrian Bowyer <adrian@reprappro.com>
2021-10-18 16:03:11 +0100

    Added code to deal with optimisation from scanning triangles.

--- Simulator/pointCalibration.py
+++ Simulator/pointCalibration.py
@@ -119,111 +119,118 @@ def GetRoomFromJamesesScan(scanFile):
    room.append(point)
  return room
 
+def FakeTriangle(scanner, triangleSideLength):
+ triangle = []
+ angle = uniform(-0.5, 0.5)
+ apex = Vector3(uniform(-500, 500), uniform(1000, 2000), uniform(-3, 3))
+ x = triangleSideLength*maths.cos(angle)
+ y = triangleSideLength*maths.sin(angle)
+ triangle.append(scanner.PointInSpaceToPixel(Vector3(apex.x - x, apex.y + y, apex.z)))
+ triangle.append(scanner.PointInSpaceToPixel(apex))
+ triangle.append(scanner.PointInSpaceToPixel(Vector3(apex.x + y, apex.y + x, apex.z)))
+ return triangle
 
-#*********************************************************************************************
+def DoTriangleOptimisation(pixelsAndAnglesJB, triangleSideLength):
 
-seed(7)
+ seed(7)
 
-world = ScannerPart()
+ world = ScannerPart()
 
-#scanner = Scanner(world, scannerOffset = Vector3(0, 0, 0), lightOffset = Vector3(36, 0, 0), lightAng = 0.454, lightToeIn = 0, cameraOffset =
-#		 Vector3(-7.75, 0, 352.0), cameraToeIn = -20.32*maths.pi/180.0, uPix = 2464, vPix = 3280, uMM = 2.76, vMM = 3.68, focalLen = 8)
-scanner = Scanner(world, scannerOffset = Vector3(0, 0, 0), lightOffset = Vector3(36, 0, 0), lightAng = 0.454, lightToeIn = 0,
+ scanner = Scanner(world, scannerOffset = Vector3(0, 0, 0), lightOffset = Vector3(36, 0, 0), lightAng = 0.454, lightToeIn = 0,
 		 cameraOffset = Vector3(-7.75, 0, 352.0), cameraToeIn = -20.32*maths.pi/180.0, uPix = 2464, vPix = 3280, uMM = 2.76, vMM = 3.68, focalLen = 8)
 
-pixelsAndAnglesJB = LoadPixelsAndAngles("RoomReaderScanDebug.txt")
-roomJB = GetRoomFromJamesesScan("RoomReaderScan.pts")
-roomAB = GetRoomFromJamesesScan("RoomReaderScanAB.pts")
+ sv = [6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+ scanner.DefineSelectionVector(sv)
+
+ print("Initial scanner:")
+ print(str(scanner))
+
+ if pixelsAndAnglesJB is None:
+  trianglePixels = []
+  angles = []
+  for t in range(6):
+   trianglePixels.append(FakeTriangle(scanner, triangleSideLength))
+   angles.append(0.0)
+  pixelsAndAnglesJB = (trianglePixels, angles)
+
+ dataCount = len(pixelsAndAnglesJB[0])
+ print("There are " + str(dataCount) + " triangles in the scan.")
+
+ scanner = scanner.MonteCarloTriangles(triangleSideLength, pixelsAndAnglesJB, 8, 3, 100)
+
+ scanner = scanner.OptimiseTriangles(triangleSideLength, pixelsAndAnglesJB)
+
+ return scanner
+
+#******************************************************************************************************************************************
+
+def DoPointsOptimisation(pixelsAndAnglesJB, roomJB):
+ seed(7)
+
+ world = ScannerPart()
+
+ scanner = Scanner(world, scannerOffset = Vector3(0, 0, 0), lightOffset = Vector3(36, 0, 0), lightAng = 0.454, lightToeIn = 0,
+		 cameraOffset = Vector3(-7.75, 0, 352.0), cameraToeIn = -20.32*maths.pi/180.0, uPix = 2464, vPix = 3280, uMM = 2.76, vMM = 3.68, focalLen = 8)
+
+ sv = [6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+ scanner.DefineSelectionVector(sv)
+
+ print("Initial scanner:")
+ print(str(scanner))
+
+ pixelsJB = pixelsAndAnglesJB[0]
+ anglesJB = pixelsAndAnglesJB[1]
+
+ dataCount = len(pixelsAndAnglesJB[0])
+ print("There are " + str(dataCount) + " pixels in the scan.")
+
+ coarse = 30
+ sample = int(dataCount/coarse)
+
+ room = []
+ pixels = []
+ angles = []
+ for s in range(0, len(pixelsJB), coarse):
+  room.append(roomJB[s])
+  pixels.append(pixelsJB[s])
+  angles.append(anglesJB[s])
+
+ shortPixelsAndAnglesJB = (pixels, angles)
+
+ print("Sampled " + str(len(pixels)) + " pixels for the optimisation.")
+
+ scanner = scanner.MonteCarloPoints(room, shortPixelsAndAnglesJB, 8, 3, 100)
+
+ scanner = scanner.OptimisePoints(room, shortPixelsAndAnglesJB)
+
+ return scanner
+#*********************************************************************************************
+
 
 '''
-scanner = Scanner(world,
-                  scannerOffset = Vector3(-863.612446147733, 2547.3621918921745, 119.50872958629937),
-                  lightOffset = Vector3(-1351.1344535599703, -660.9864964136729, -122.71266770495758),
-                  lightAng = 0.454,
-                  lightToeIn = -0.2509728303244617,
-                  cameraOffset = Vector3(519.652379926871, 3209.7700491595915, 598.1087874460901),
-                  cameraToeIn = -0.32785573647400157,
-                  uPix = 2464, vPix = 3280, uMM = 2.76, vMM = 3.68,
-                  focalLen = 18.944401990122746)
-
-#scanner = Scanner(world, scannerOffset = Vector3(0, 0, 0), lightOffset = Vector3(36, 0, 23.15), lightAng = 0.454, lightToeIn = 0, cameraOffset =
-#		 Vector3(-24.8, 0, 436.0), cameraToeIn = -10.94*maths.pi/180.0, uPix = 2464, vPix = 3280, uMM = 2.76, vMM = 3.68, focalLen = 25)
-
-pixel = (0, 2573.0036496350367)
-#pixel = (2464/2.0, 3280/2.0)
-#pixel = (0, 0)
-point = Vector3(-188.85944094399068, 1348.9119290579144, 0)
-scanner.CheckPoint(point, pixel, True)
-#scanner.CheckCornersAndMiddle()
-
-points = [
- Vector3(-50, 1000, 0),
- Vector3(0, 1000, 0),
- Vector3(50, 1000, 0),
- Vector3(-50, 2000, 0),
- Vector3(0, 2000, 0),
- Vector3(50, 2000, 0)
-]
-for point in points:
- scanner.CheckPoint(point, None, True)
- print()
-
-
-sv = [6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-
-scanner.DefineSelectionVector(sv)
-
-print("Initial scanner:")
-print(str(scanner))
-
-roomJB = GetRoomFromJamesesScan("RoomReaderScan-JB.pts")
-scanner.SelfCheck(roomJB)
-
-pixelsAndAnglesJB = LoadPixelsAndAngles("RoomReaderScanCamera1-pixels.txt")
-pixelsJB = pixelsAndAnglesJB[0]
-anglesJB = pixelsAndAnglesJB[1]
-
-dataCount = len(pixelsAndAnglesJB[0])
-print("There are " + str(dataCount) + " pixels in the scan.")
-
-coarse = 30
-sample = int(dataCount/coarse)
-
-room = []
-pixels = []
-angles = []
-for s in range(0, len(pixelsJB), coarse):
- room.append(roomJB[s])
- pixels.append(pixelsJB[s])
- angles.append(anglesJB[s])
-
-shortPixelsAndAnglesJB = (pixels, angles)
-
-print("Sampled " + str(len(pixels)) + " pixels for the optimisation.")
-
-#betterScanner = scanner.MonteCarlo(room, shortPixelsAndAnglesJB, 8, 3, 100)
-
-#bestScanner = betterScanner.Optimise(room, shortPixelsAndAnglesJB)
-
-
-parameters = [0, 0, 0, 36, 0, 0, 36.29020647040032, -64.73034918263649, 347.2584403909511, 9.132270456355052, 0.0, 2.536641779027073, 
- 0.09830280745876545, 0.0014337786420365016, 0.007458111749965841, 3.344304399002978, 6.270129696264745, 1.5628220387349634, 
- 6.283153237497993, 6.283152870577741, 6.221876183673617]
-
-bestScanner = scanner.Copy()
-bestScanner.ImposeParameters(parameters)
-'''
-
-
-#parameters = [0, 0, 0, 36, 0, 0, 36.29020647040032, -64.73034918263649, 347.2584403909511, 9.132270456355052, 0.0, 2.536641779027073, 0.09830280745876545, 0.0014337786420365016, 0.007458111749965841, 3.344304399002978, 6.270129696264745, 1.5628220387349634, 6.283153237497993, 6.283152870577741, 6.221876183673617]
-
-
 parameters = [0, 0, 0, 36, 0, 0, 36.29020647040032, -64.73034918263649, 347.2584403909511, 9.132270456355052, 0.0, 2.536641779027073, 0.09830280745876545, 0.0014337786420365016, 0.007458111749965841, 3.344304399002978, 6.270129696264745, 1.5628220387349634, 6.283153237497993, 6.283152870577741, 6.221876183673617]
 
 scanner.ImposeParameters(parameters)
+roomAB = GetRoomFromJamesesScan("RoomReaderScanAB.pts")
+'''
+def Room():
+ pixelsAndAnglesJB = LoadPixelsAndAngles("RoomReaderScanDebug.txt")
+ roomJB = GetRoomFromJamesesScan("RoomReaderScan.pts")
+ scanner = DoPointsOptimisation(pixelsAndAnglesJB, roomJB)
 
-print("Final scanner:")
-print(str(scanner))
-recoveredRoom = scanner.ReconstructRoomFromPixelsAndAngles(pixelsAndAnglesJB)
+ print("Final scanner:")
+ print(str(scanner))
+ recoveredRoom = scanner.ReconstructRoomFromPixelsAndAngles(pixelsAndAnglesJB)
 
-PlotRooms(roomJB, roomAB,"scan.png", 0.5)
+ PlotRooms(roomJB, recoveredRoom, None, 0.5)
+
+def Triangles():
+ scanner = DoTriangleOptimisation(None, 200)
+ print("Final scanner:")
+ print(str(scanner))
+
+
+Triangles()
+#Room()
